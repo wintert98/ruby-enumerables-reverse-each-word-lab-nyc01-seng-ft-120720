bf980a9df32e8fc5cf4f81dc5260144ff2f475f3
Thomas Winter <wintert98@gmail.com>
2020-11-28 18:30:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+#def reverse_each_word(sentence)
+#  s_arr = sentence.split(" ")
+# new_arr = []
+#  s_arr.each do |word|
+#   new_arr << word.reverse
+#  end
+#  new_arr = new_arr.join(" ")
+#  new_arr
+#end
+
+def reverse_each_word(sentence)
+  s_arr = sentence.split(" ")
+  
+  s_arr.collect do |word|
+    word.reverse!
+  end
+  s_arr = s_arr.join(" ")
+  s_arr
+end
